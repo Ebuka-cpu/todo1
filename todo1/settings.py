e7c17965a2204f376d52de00e77c67a591d1b39c
Ebuka-cpu <ebukafelixanyaegbu@gmail.com>
2020-07-01 13:24:15 +0100

    Updated settings

--- todo1/settings.py
+++ todo1/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-pzy8906t^pw=ho3edv$!95h7mcq2xwiwont8f8q!fy@3c)n7^'
+# SECRET_KEY = '-pzy8906t^pw=ho3edv$!95h7mcq2xwiwont8f8q!fy@3c)n7^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myawsomedjangotodoapp.herokuapp.com']
 
 
 # Application definition
